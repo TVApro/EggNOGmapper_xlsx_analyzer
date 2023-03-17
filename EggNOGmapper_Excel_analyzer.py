2895612a8c4f488e9ea6e5e430c5c88668cd4469
Vladimir Trubitsyn <75006854+TVApro@users.noreply.github.com>
2023-03-17 11:10:00 +0300

    Update EggNOGmapper_Excel_analyzer.py

--- EggNOGmapper_Excel_analyzer.py
+++ EggNOGmapper_Excel_analyzer.py
@@ -1,18 +1,16 @@
 #! /usr/bin/python3
 # Программа для исследования результатов работы EggNOG
 
-import re
-from re import sub
-import textwrap as tw
 import pandas as pd
-import openpyxl
 import os
 from collections import Counter
 
-path = input("Укажите путь до папки с таблицами: ")
-strange = input("Считать функциональные группы COG? (y/n) ")
-if strange == 'n':
-    column_x = input("Имя колонки для подсчёта значений")
+#path = input("Укажите путь до папки с таблицами: ")
+path = '/EggNOG/Tables'
+#description = input("Введите название столбца, по которому хотите осуществить поиск? Проверьте название заранее: ")
+description = 'Description'
+#COG_group = input("Введите название группы COG, в пределах которой хотите осуществить поиск: ")
+COG_group = 'C'
 results_list = os.listdir(path)
 n = 0
 for i in results_list:
@@ -21,19 +19,23 @@ for i in results_list:
     i_name = i.replace('.xlsx', '')
     table = pd.ExcelFile(path_i)
     df1 = table.parse('Sheet1') # исходную таблицу преобразовали в объект Pandas
-    if strange == 'y':
-        COG_df = df1['COG_category'].value_counts() # подсчёт значений в столбце COG_category
-    if strange == 'n':
-        COG_df = df1[column_x].value_counts() # подсчёт значений в любом другом столбце
+    if COG_group:
+        COG_df1 = df1.loc[df1['COG_category'] == COG_group]
+        COG_df2 = COG_df1[description].value_counts() # подсчёт значений в столбце COG_category
+    else:
+        COG_df2=df1[description].value_counts()
     if n == 1:
-        df = COG_df.to_frame()  # превращаем Series в DataFrame (создаём заново)
+        df = COG_df2.to_frame()  # превращаем Series в DataFrame (создаём заново)
         df.columns = [i_name] # имя колонки - название таблицы
     if n > 1:
-        new_df = COG_df.to_frame() # превращаем Series в DataFrame (доращиваем)
+        new_df = COG_df2.to_frame() # превращаем Series в DataFrame (доращиваем)
         new_df.columns = [i_name] # имя колонки - название таблицы
         df = pd.concat([df, new_df], axis = 1) # объединяем датафреймы
 print(df)
-if strange == 'n':
-    export = input('Под каким именем вы хотите сохранить результат? ')
-    path_export = os.path.join(path, export)
+
+export = input('Имя для сохранения (если нужно):')
+if export:
+    export_name = export + '.csv'
+    path_export = os.path.join(path, export_name)
     df.to_csv(path_export)
+    print('Сохранён файл', path_export)
