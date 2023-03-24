230b8482d662602e5a7550fd3175a431e501c4f2
Vladimir Trubitsyn <75006854+TVApro@users.noreply.github.com>
2023-03-24 12:26:59 +0300

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
-
+path=$(pwd)
 folder1=Tables_Prodigal
-
 folder2=_1_Total_number_of_genes_in_groups
 folder3=_2_Quantifiably_changing_groups
 folder4=_3_Genes_subsets_in_quantifiably_changing_groups
 
-if [! -d {pwd}/${folder2}]; then
+if [ ! -d "${path}"/$folder1${folder2} ]; then
+	echo Выполняется скрипт 1
 	python3 ADT_1.py ${folder1}
-else
-	echo 'Папка ${folder2} уже существует. Пропуск'
+else 
+	echo Папка ${folder1}${folder2} уже существует. Пропуск
 fi
+echo -n '////////'
 
-if [! -d {pwd}/${folder3}]; then
+if [ ! -d "${path}"/${folder1}${folder3} ]; then
+	echo Выполняется скрипт 2
 	python3 ADT_2.py ${folder2} ${folder1}
 else
-	echo 'Папка ${folder3} уже существует. Пропуск'
+	echo Папка ${folder1}${folder3} уже существует. Пропуск
 fi
+echo -n '////////'
 
-if [! -d {pwd}/${folder4}]; then
+if [ ! -d "${path}"/${folder1}${folder4} ]; then
 	python3 ADT_3.py ${folder3} ${folder1}
 else
-	echo 'Папка ${folder3} уже существует. Пропуск'
+	echo Папка ${folder1}${folder3} уже существует. Пропуск
 fi
 
-python3 ADT_4.py ${folder4} ${folder1}
+echo end!
